1db9c115087e45dabed9f9e9fe2454c663e397e4
LarkXa <larkxa@gmail.com>
2024-11-18 16:42:25 +0800

    master

---
+++ src/views/Main.vue
@@ -0,0 +1,28 @@
+<!-- <template>
+    <div>
+        Layout
+    </div>
+</template>
+<script setup>
+
+</script> -->
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+          <!-- <el-aside width="200px">Aside</el-aside> -->
+           <Aside />
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+<script setup>
+import Aside from '../components/aside.vue'
+</script>  
+<style lang="less" scoped>
+    .common-layout{
+        height: 100%;
+    }
+</style>
